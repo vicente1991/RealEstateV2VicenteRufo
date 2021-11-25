225e0ffffb059ce68108eb477e31cc1652c829f7
vicente <etenciv@gmail.com>
2021-11-25 18:03:18 +0100

    arreglando main

--- RealEstate/src/main/java/com/salesianostriana/dam/RealEstate/repository/InmobiliariaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salesianostriana.dam.RealEstate.repository;
-
-import com.salesianostriana.dam.RealEstate.model.Inmobiliaria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InmobiliariaRepository extends JpaRepository<Inmobiliaria, Long> {
-}
